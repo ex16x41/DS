ee1b2ba277b5f78e363cdac0256d6f429d03540d
Eva Prokofiev <44981946+ex16x41@users.noreply.github.com>
2023-03-12 01:51:35 +0100

    Update run.js

--- run.js
+++ run.js
@@ -1,43 +1,43 @@
 const dorkCategories = {
-	"Misconfigured Interfaces": [
-		"intitle:index.of",
-		"inurl:.svn",
-		"inurl:/owa/auth/logon.aspx",
-		"inurl:/admin/login.php",
-		"inurl:/wp-admin/ intext:username",
-		"inurl:/admin/login.php filetype:php",
-		"inurl:/wp-admin/ filetype:php"
+	'Misconfigured Interfaces': [
+		'intitle:index.of',
+		'inurl:.svn',
+		'inurl:/owa/auth/logon.aspx',
+		'inurl:/admin/login.php',
+		'inurl:/wp-admin/ intext:username',
+		'inurl:/admin/login.php filetype:php',
+		'inurl:/wp-admin/ filetype:php'
 	],
-	"Exposed Documents": [
-		"filetype:php",
-		"site:{domain} intext:password",
-		"intext:username filetype:log",
-		"inurl:wp-content/uploads",
-		"intext:DB_PASSWORD",
-		"intitle:\"Index of\" passwords.txt",
-		"site:{domain} ext:sql | ext:dbf | ext:mdb",
-		"intext:\"sql syntax near\" | intext:\"syntax error has occurred\" filetype:php",
-		"inurl:backup filetype:mdb",
-		"site:{domain} ext:xml | ext:conf | ext:cnf | ext:reg | ext:inf | ext:rdp | ext:cfg | ext:txt | ext:ora | ext:ini"
+	'Exposed Documents': [
+		'filetype:php',
+		'site:{domain} intext:password',
+		'intext:username filetype:log',
+		'inurl:wp-content/uploads',
+		'intext:DB_PASSWORD',
+		'intitle:"Index of" passwords.txt',
+		'site:{domain} ext:sql | ext:dbf | ext:mdb',
+		'intext:"sql syntax near" | intext:"syntax error has occurred" filetype:php',
+		'inurl:backup filetype:mdb',
+		'site:{domain} ext:xml | ext:conf | ext:cnf | ext:reg | ext:inf | ext:rdp | ext:cfg | ext:txt | ext:ora | ext:ini'
 	],
-	"Sensitive Directories": [
-		"intitle:index.of",
-		"intitle:\"Index of /\" +{directory}",
-		"intitle:\"Index of /\" inurl:{directory}"
+	'Sensitive Directories': [
+		'intitle:index.of',
+		'intitle:"Index of /" +{directory}',
+		'intitle:"Index of /" inurl:{directory}'
 	]
 	// add more categories and dork queries as needed
 };
 
 function searchDorks(event) {
 	event.preventDefault();
-	const category = document.getElementById("category").value;
-	const domain = document.getElementById("domain").value;
+	const category = document.getElementById('category').value;
+	const domain = document.getElementById('domain').value;
 	const dorks = dorkCategories[category];
 	const searchResults = [];
 
 	dorks.forEach(dork => {
-		const query = dork.replace("{domain}", domain);
-		const searchUrl = "https://www.google.com/search?q=" + encodeURIComponent(query);
+		const query = dork.replace('{domain}', domain);
+		const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(query);
 		searchResults.push({
 			query: dork,
 			url: searchUrl
